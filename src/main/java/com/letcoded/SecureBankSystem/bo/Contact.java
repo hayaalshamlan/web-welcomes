99dea5069379defd70d6eea12bce0c098bdc9bee
Haya A S Sh M Alshamlan <83336@kfh.com>
2024-02-04 15:46:31 +0300

    save the update

---
+++ src/main/java/com/letcoded/SecureBankSystem/bo/Contact.java
@@ -0,0 +1,34 @@
+package com.letcoded.SecureBankSystem.bo;
+
+public class Contact {
+
+    public static String name;
+    public static String email;
+    public static String phone;
+
+    public static String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public static String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public static String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+
+}
--- src/main/java/com/letcoded/SecureBankSystem/controller/CodedController.java
+++ src/main/java/com/letcoded/SecureBankSystem/controller/CodedController.java
@@ -1,11 +1,16 @@
 package com.letcoded.SecureBankSystem.controller;
 
+import com.letcoded.SecureBankSystem.bo.Contact;
 import com.letcoded.SecureBankSystem.bo.CreateFarewellRequest;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 @RestController
 public class CodedController {
+    List<Contact> contasts = new ArrayList<>();
 
     @GetMapping("/sayHi")
     public String sayHi() {
@@ -21,4 +26,16 @@ public class CodedController {
     public String Farewell(@RequestBody CreateFarewellRequest createFarewellRequest) {
         return "GoodBye, " + createFarewellRequest.getName();
     }
+
+    @PostMapping("/AddContact")
+    public String AddContact(@RequestBody Contact contact) {
+        contasts.add(contact);
+        return "AddContent " + "name: " + contact.getName() + " " + "email: " + " " + contact.getEmail() + " " + "phone: " + contact.getPhone();
+    }
+
+    @GetMapping("/GetContactDetails")
+    public String GetContactDetails(@RequestBody Contact contact) {
+        return "GetContactDetails" + " " + "name: " + contact.getName() + " " + "email: " + " " + contact.getEmail() + " " + "phone: " + contact.getPhone();
+    }
+
 }
